249d4115276b4c79a12615e0a76ecf09ee2d9b63
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2026-03-15 18:37:01 +0530

    update

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,18 +1,17 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  service: "gmail",
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
-
 });
-
 const sendEmail = async (to, subject, html) => {
 
   await transporter.sendMail({
