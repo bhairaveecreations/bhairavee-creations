94f67b2c1c155875b7cfd05cebb1aff3d33e85ae
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2026-03-15 16:50:35 +0530

    change

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
     <main>
       <Hero />
       <SignatureCollection/>
-      <BrandHighlights/>
+      {/* <BrandHighlights/> */}
       <CustomOrderCTA/>
       <Testimonials/>
       <InstagramSection/>
