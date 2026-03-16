f75b1e12c3f911e68b2eee2e9976d26ba9c47427
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2026-03-16 08:06:30 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -11,13 +11,18 @@ connectDB();
 
 const app = express();
 
+app.set("trust proxy", 1);
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      "http://localhost:3000",
+      process.env.FRONTEND_URL
+    ],
     credentials: true
   })
 );
@@ -28,15 +33,18 @@ app.use(
     max: 100
   })
 );
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
+
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/products", require("./routes/productRoutes"));
 app.use("/api/orders", require("./routes/orderRoutes"));
 app.use("/api/admin", require("./routes/adminRoutes"));
 app.use("/api/custom-orders", require("./routes/customOrderRoutes"));
 app.use("/api/payment", require("./routes/paymentRoutes"));
+
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
