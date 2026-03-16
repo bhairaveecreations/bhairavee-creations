ba78cd20eb00f7873b4cc20f6f4ccd0d13a16010
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2026-03-16 09:19:26 +0530

    commit

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -8,9 +8,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  tls: {
-    rejectUnauthorized: false
-  }
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 });
 const sendEmail = async (to, subject, html) => {
 
