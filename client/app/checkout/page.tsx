d7b59687b0e646f4ff82b8a73f5a393fa8c946d5
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2026-03-25 22:54:22 +0530

    commit

--- client/app/checkout/page.tsx
+++ client/app/checkout/page.tsx
@@ -77,11 +77,9 @@ export default function CheckoutPage() {
     setForm({ ...form, pincode: value });
 
     if (value.length === 6) {
-      if(value.startsWith("000")){
-        setDeliveryFee(0);
-      }
+
       if (value.startsWith("411") || value.startsWith("412")) {
-        setDeliveryFee(80);
+        setDeliveryFee(0);
       } else {
         setDeliveryFee(150);
       }
