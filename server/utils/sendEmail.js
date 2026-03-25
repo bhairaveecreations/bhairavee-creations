6e1a9f9a07b5af6f71e26b14d68d0c2c66b47be6
TinkeshDeshmukh <anuragdeshmukh108@gmail.com>
2026-03-25 11:11:49 +0530

    update

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,7 +1,3 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const sendEmail = async (to, subject, html) => {
   try {
     const response = await resend.emails.send({
@@ -12,9 +8,10 @@ const sendEmail = async (to, subject, html) => {
     });
 
     console.log("Email sent:", response);
+    return response;
+
   } catch (error) {
     console.error("Email failed:", error);
+    throw error; // ✅ CRITICAL FIX
   }
 };
-
-module.exports = sendEmail;
